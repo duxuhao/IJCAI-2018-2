9b25753b1863a6b31fe6ab31eb15eea1adfe880a
duxuhao <duxuhao88@gmail.com>
2018-04-06 02:36:48 +0000

    update 008075

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -82,8 +82,8 @@ if __name__ == "__main__":
                    '*':times,
                    '/':divide,}
 
-    RecordFolder = 'record_newselection_lgb5_train2.log'
-    modelselect = 'lgb5'
+    RecordFolder = 'record_lgb6.log'
+    modelselect = 'lgb6'
 
     temp = ['item_category_list', 'item_price_level',
                   'item_sales_level',
@@ -152,4 +152,5 @@ if __name__ == "__main__":
                   'check_item_brand_id_time_day'
                  ]
     temp = ['user_occupation_id_tradexd','user_occupation_id_item_price_level_tradexd','user_age_level_shop_review_num_level_tradexd','user_id_shop_id_tradexd','user_id_tradexd','item_category_list', 'item_price_level', 'item_sales_level', 'item_collected_level', 'item_pv_level', 'user_gender_id', 'user_age_level', 'user_occupation_id', 'user_star_level', 'context_page_id', 'shop_review_num_level', 'shop_review_positive_rate', 'shop_score_service', 'shop_score_delivery','hour', 'day', 'user_id_query_day_hour', 'shop_id', 'item_id_query_day', 'user_id_query_day_item_brand_id', 'user_id_query_day_hour_item_brand_id', 'user_id_query_day', 'item_brand_id', 'user_id_query_day_item_id', 'check_item_brand_id_ratio', 'check_shop_id_ratio', 'check_item_category_list_ratio', 'check_ratio_day_all', 'check_time_day', 'item_city_id_shop_cnt', 'item_city_id_shop_rev_prob', 'item_id_shop_rev_cnt', 'item_property_list0', 'item_pv_level_price_prob', 'item_collected_level_item_prob', 'item_sales_level_price_prob']
+    temp = ['item_category_list', 'item_price_level', 'item_sales_level', 'item_collected_level', 'user_gender_id', 'user_age_level', 'user_star_level', 'context_page_id', 'shop_review_positive_rate', 'shop_score_service', 'hour', 'day', 'user_id_query_day_hour', 'shop_id', 'user_id_query_day_item_brand_id', 'user_id_query_day_hour_item_brand_id', 'user_id_query_day', 'user_id_query_day_item_id', 'check_item_brand_id_ratio', 'check_shop_id_ratio', 'check_item_category_list_ratio', 'check_ratio_day_all', 'item_city_id_shop_cnt', 'item_city_id_shop_rev_prob', 'item_id_shop_rev_cnt', 'item_property_list0', 'item_pv_level_price_prob', 'item_collected_level_item_prob', 'item_sales_level_price_prob', 'item_city_id_cnt1d', 'item_collected_level_user_age_cnt', 'item_price_level_item_cnt', 'item_price_level_item_prob', 'user_id_shop_id_trade_meanxd', 'user_age_level_trade_meanxd','user_id_item_category_list_tradexd']
     main(temp,model[modelselect], CrossMethod, RecordFolder,test=False)
